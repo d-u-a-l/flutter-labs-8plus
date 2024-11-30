db86f7ae8f7557aef1ad2b12df9c90f299bb30b3
d.u.a.l <d.u.a.l.gm@gmail.com>
2024-11-30 21:05:14 +0200

    lab13

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import './widgets/sign_in_screen.dart';
+import 'package:intl/intl.dart';
+
+import 'database_helper.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,14 +15,117 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(
-            seedColor: Colors.blue,
-          ),
-          useMaterial3: false,
-          inputDecorationTheme: const InputDecorationTheme(
-            border: OutlineInputBorder(),
-          )),
-      home: const SignInScreen(),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const NotesScreen(),
+    );
+  }
+}
+
+class NotesScreen extends StatefulWidget {
+  const NotesScreen({super.key});
+
+  @override
+  State<NotesScreen> createState() => _NotesScreenState();
+}
+
+class _NotesScreenState extends State<NotesScreen> {
+  final TextEditingController _controller = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  List<Map<String, dynamic>> _notes = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadNotes();
+  }
+
+  Future<void> _loadNotes() async {
+    final notes = await DatabaseHelper.instance.getNotes();
+    setState(() {
+      _notes = notes;
+    });
+  }
+
+  Future<void> _addNote() async {
+    if (_formKey.currentState!.validate()) {
+      await DatabaseHelper.instance.addNote(_controller.text);
+      _controller.clear();
+      _loadNotes();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Flutter Notes App'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 8, top: 8, bottom: 8, right: 8),
+        child: Column(
+          children: [
+            Form(
+              key: _formKey,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      controller: _controller,
+                      decoration: const InputDecoration(
+                        labelText: 'Enter a note',
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.trim().isEmpty) {
+                          return 'Value is required';
+                        }
+                        return null;
+                      },
+                    ),
+                  ),
+                  const SizedBox(width: 5),
+                  ElevatedButton(
+                    onPressed: _addNote,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.deepPurple,
+                      foregroundColor: Colors.white,
+                    ),
+                    child: const Text('Add'),
+                  ),
+                ],
+              ),
+            ),
+            const Divider(
+              height: 20,
+              thickness: 0.5,
+              indent: 0,
+              endIndent: 0,
+              color: Colors.grey,
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _notes.length,
+                itemBuilder: (context, index) {
+                  final note = _notes[index];
+                  return Card(
+                    child: ListTile(
+                      title: Text(note['text']),
+                      subtitle: Text(
+                        DateFormat('dd-MM-yyyy HH:mm:ss')
+                            .format(DateTime.parse(note['createdAt'])),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
